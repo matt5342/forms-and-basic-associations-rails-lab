695b9e68d12ac2a254a080eda3f9241c0ae491f4
Matt <mattfsewell@gmail.com>
2021-02-08 06:32:34 -0500

    commit tree bf8140decb0c4fb999482496c9e19b45466d6b6b

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,11 @@
 class Note < ActiveRecord::Base
   # add associations here
+  belongs_to :song
+
+  def note_ids=(ids)
+    ids.each do |id|
+      note = Note.find(id)
+      self.notes << note
+    end
+  end
 end
